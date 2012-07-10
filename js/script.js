88b57547a4d923f99e20fc94c272f8c07f03aae9
jnoga <joseba.nogales@gmail.com>
2012-07-10 10:35:45 +0200

    retokiak

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
-/* Author:
+/* Author: Gukere
 
 */
+$(document).ready(function() {
+   // do stuff when DOM is ready
+   $(".section-link").click(function(e) {
+   		goToByScroll($(this).prop('id') + '-section');
+	});
+});
+
+function goToByScroll(id){
+	$('html,body').animate({scrollTop: $("#"+id).offset().top},'slow');
+};
 
 
 
